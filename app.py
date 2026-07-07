7056e61ce68c7f9b2d3cf2404f515359eb5bc8fa
davebruce1981-dev <davebruce1981@gmail.com>
2026-07-07 19:11:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import base64
 st.set_page_config(page_title="Recovery Specs", layout="centered")
 
 # CENTRALIZED URL FOR THE GOOGLE APPS SCRIPT
-GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzcjYzl5kmbGMfy90KXxO8b18E-eWYK-Xc9EAOxwROFtDoOQHePYduTXMEfiTarb7Jh/exec"
+GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbygK24ZYkFxKAcS78YM5mSVbLVsw6BkvUoiVkMUgc_w6NYBC9L1JzCjTC4cxsGbFMRftw/exec"
 
 st.markdown("""
     <style>
